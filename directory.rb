7460328c4f577a6f70ab6e54c8a31d6856e352bb
Matthew Lock <matthew_lock20@hotmail.com>
2020-05-29 14:05:15 +0100

    User Input

--- directory.rb
+++ directory.rb
@@ -1,17 +1,4 @@
 #first we print the list of students
-students = [
-{name: 'Dr Hannibal Lecter', cohort: :november},
-{name: 'Darth Vader', cohort: :november},
-{name: 'Nurse Ratched', cohort: :november},
-{name: 'Michael Corleone', cohort: :november},
-{name: 'Alex DeLarge', cohort: :november}, 
-{name: 'The Wicked Witch of the West', cohort: :november},
-{name: 'Terminator', cohort: :november},
-{name: 'Freddy Krueger', cohort: :november},
-{name: 'The Joker', cohort: :november},
-{name: 'Joffrey Baratheon', cohort: :november},
-{name: 'Norman Bates', cohort: :november},
-]
 def print_header
   puts 'The students of Villians Academy'
   puts '-------------'
@@ -27,7 +14,23 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  students =[]
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
 # nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
