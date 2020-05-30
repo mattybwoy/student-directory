9d0551aee4a0841480876713ea1b3bb885b83f13
Matthew Lock <matthew_lock20@hotmail.com>
2020-05-30 18:39:52 +0100

    Adding Default Load File

--- directory.rb
+++ directory.rb
@@ -1,9 +1,17 @@
-@students = []
+@students = [] #empty array accessible to all methods
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit"
+end
 
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -24,12 +32,36 @@ def process(selection)
   end
 end
 
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit"
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  name = STDIN.gets.chomp
+  while !name.empty? do
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    name = STDIN.gets.chomp
+  end
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def print_header
+  puts 'The students of Villians Academy'
+  puts '-------------'
+end
+
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 def save_students
@@ -42,8 +74,8 @@ def save_students
   file.close  
 end
 
-def load_students
-  file = File.open("students.csv","r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
     @students << {name: name, cohort: cohort.to_sym}
@@ -51,40 +83,18 @@ def load_students
   file.close
 end
 
-def show_students
-  print_header
-  print_students_list
-  print_footer
-end
-
-def print_header
-  puts 'The students of Villians Academy'
-  puts '-------------'
-end
-
-def print_students_list
-  @students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+def try_load_students
+  filename = ARGV.first #first argument from the command line
+  return if filename.nil? #get out if filename doesn't exist
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry #{filename} doesn't exist."
+    exit
   end
 end
-
-def print_footer
-  puts "Overall, we have #{@students.count} great students"
-end
-
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  name = gets.chomp
-
-  while !name.empty? do
-    @students<< {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
-    name = gets.chomp
-  end
-  @students
-end
-
 # nothing happens until we call the methods
+try_load_students
 interactive_menu
 
