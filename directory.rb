0a19702021efc1bb522b0dbc951b9590f95199f0
Matthew Lock <matthew_lock20@hotmail.com>
2020-05-30 10:44:15 +0100

    Refactoring Process

--- directory.rb
+++ directory.rb
@@ -2,28 +2,34 @@
 
 def interactive_menu
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # 2. read the input and put it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection  
     when "1"
-      students = input_students
-      #input the students
+      input_students
     when "2"
-      print_header
-      print(students)
-      print_footer(students)
-      #show the students
+      show_students
     when "9"
-      exit #this will cause the program to terminate
+      exit
     else
       puts " I don't know what you meant, try again"
   end
 end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
 def print_header
@@ -31,13 +37,13 @@ def print_header
   puts '-------------'
 end
 
-def print(students)
+def print_students_list
   @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
+def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
@@ -45,7 +51,7 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   name = gets.chomp
-  
+
   while !name.empty? do
     @students<< {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
